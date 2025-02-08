ab0b180c48033235946f705d88640eb61756b0da
Jorge <jorge.garram95@gmail.com>
2025-02-08 06:08:26 +0100

    Añadido Javadoc a CCuenta

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,16 +1,29 @@
 package cuentas;
-
+/**
+ * Clase que describe una cuenta bancaria
+ * Permitirá la gestión del nombre del titular, el número de cuenta, el saldo y el tipo de Interés, así como ingresar/retirar dinero
+ * @author jorge
+ */
 public class CCuenta {
-
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
+    /**
+     * Constructor de la clase CCuenta, inicializa la cuenta sin valores
+     */
     public CCuenta(){
     }
 
+    /**
+     * Inicializa los atributos de la cuenta con los valores que se proporcionen:
+     * @param nom Nombre del titular de la cuenta
+     * @param cue Número de la cuenta
+     * @param sal Saldo inicial
+     * @param tipo Tipo de interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -19,7 +32,13 @@ public class CCuenta {
         tipoInterés = tipo; // Agregamos el constructor de tipoInterés ya que faltaba
     }
 
-    // Creamos los getter/setter para cada atributo:
+    /**
+     * Métodos getter/setter:
+     * Getter: Obtendrá el nombre/número de cuenta/saldo/tipo de interés (dependiendo del método que usemos)
+     * Setter: Establecerá el nombre/número de cuenta/saldo/tipo de interés (dependiendo del método que usemos)
+     */
+    
+        // Creamos los getter/setter para cada atributo:
     
     // Nombre:
     public String getNombre() { return nombre; }
@@ -37,12 +56,22 @@ public class CCuenta {
     public double getTipoInterés() { return tipoInterés; }
     public void setTipoInterés(double tipoInterés) { this.tipoInterés = tipoInterés; }
     
-   
+   /**
+    * Nos devolverá el saldo actual de la cuenta
+    * @return saldo actual
+    */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Método para ingresar una cantidad de dinero
+     * Si la cantidad resulta negativa, lanzará una excepción
+     * @param cantidad - Cantidad a ingresar
+     * @throws Exception en caso de cantidad negativa
+     */
+    
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -50,6 +79,13 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    
+    /**
+     * Método para retirar dinero
+     * @param cantidad - cantidad a retirar
+     * @throws Exception en caso de que el saldo sea insuficiente o la cantidad sea negativa
+     */
+    
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -3,11 +3,13 @@ package cuentas;
 public class Main {
 
     public static void main(String[] args) {
-        operativa_cuenta(); // Llamado al método operativa_cuenta
+        // Actualizaremos el llamado a operativa_cuenta, creando un float cantidadOperacion para hacer más sencilla la modificación futura del código
+        float cantidadOperacion = 500;
+        operativa_cuenta(cantidadOperacion); // Llamado al método operativa_cuenta
     }
     
     // Creamos el nuevo método operativa_cuenta, donde quedarán todas las operaciones con cuenta1 que anteriormente estaban directamente dentro del método main
-    public static void operativa_cuenta() {
+    public static void operativa_cuenta(float cantidad) { // Añadimos float cantidad como parámetro a operativa_cuenta
         CCuenta cuenta1;
         double saldoActual;
 
@@ -15,14 +17,16 @@ public class Main {
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
+    // Tanto en retirar como ingresar sustituiremos las cantidades por "cantidad"    
+        
         try {
-            cuenta1.retirar(2300);
+            cuenta1.retirar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            cuenta1.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
